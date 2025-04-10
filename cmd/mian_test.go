5f932ad89f0bc8899fdb2d1b80f8c517298ab04d
nabishec <nabishec@mail.ru>
2025-04-10 21:18:29 +0300

    add indexes to db and integration test

---
+++ cmd/mian_test.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+
+	dbconnection "github.com/nabishec/avito_pvz_service/cmd/db_connection"
+	"github.com/nabishec/avito_pvz_service/internal/model"
+	"github.com/nabishec/avito_pvz_service/internal/storage/db"
+)
+
+func executeRequest(req *http.Request, s *Server) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	s.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func testGetTokenForClient(t *testing.T, s *Server, role string) string {
+	reqBody := map[string]string{
+		"role": role,
+	}
+
+	jsonReq, err := json.Marshal(reqBody)
+	if err != nil {
+		t.Fatal("failed to marshal request body")
+	}
+
+	req, _ := http.NewRequest("POST", "/dummyLogin", bytes.NewBuffer(jsonReq))
+	req.Header.Set("Content-Type", "application/json")
+
+	responce := executeRequest(req, s)
+	checkResponseCode(t, http.StatusOK, responce.Code)
+
+	var authResp model.AuthResponse
+	err = json.Unmarshal(responce.Body.Bytes(), &authResp)
+	if err != nil {
+		t.Fatal("failed to unmarshal response body")
+	}
+	return authResp.Token
+}
+
+func TestService(t *testing.T) {
+	err := LoadEnv()
+	if err != nil {
+		t.Error("Don't found config")
+	}
+
+	dbConnection, err := dbconnection.NewDatabaseConnection()
+	if err != nil {
+		t.Error("Failed init database")
+	}
+
+	storage := db.NewStorage(dbConnection.DB)
+
+	s := CreateNewServer(storage)
+	s.MountHandlers()
+	tokenClient := testGetTokenForClient(t, s, "client")
+	tokenModerator := testGetTokenForClient(t, s, "moderator")
+	var pvzResp model.PVZResp
+
+	t.Run("Add PVZ", func(t *testing.T) {
+		reqBody := map[string]string{
+			"city": "Санкт-Петербург",
+		}
+
+		jsonReq, err := json.Marshal(reqBody)
+		if err != nil {
+			t.Fatal("failed to marshal request body")
+		}
+
+		req, _ := http.NewRequest("POST", "/pvz", bytes.NewBuffer(jsonReq))
+		req.Header.Set("Authorization", "Bearer "+tokenModerator)
+		req.Header.Set("Content-Type", "application/json")
+
+		response := executeRequest(req, s)
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+		err = json.Unmarshal(response.Body.Bytes(), &pvzResp)
+		if err != nil {
+			t.Fatal("failed to unmarshal response body")
+		}
+	})
+
+	t.Run("Add Reception", func(t *testing.T) {
+		reqBody := map[string]string{
+			"pvz_id": pvzResp.ID.String(),
+		}
+
+		jsonReq, err := json.Marshal(reqBody)
+		if err != nil {
+			t.Fatal("failed to marshal request body")
+		}
+
+		req, _ := http.NewRequest("POST", "/receptions", bytes.NewBuffer(jsonReq))
+		req.Header.Set("Authorization", "Bearer "+tokenClient)
+		req.Header.Set("Content-Type", "application/json")
+
+		response := executeRequest(req, s)
+		checkResponseCode(t, http.StatusCreated, response.Code)
+
+	})
+
+	t.Run("Add Product", func(t *testing.T) {
+		var wg sync.WaitGroup
+
+		reqBody := map[string]string{
+			"pvz_id": pvzResp.ID.String(),
+			"type":   "обувь",
+		}
+
+		jsonReq, err := json.Marshal(reqBody)
+		if err != nil {
+			t.Fatal("failed to marshal request body")
+		}
+
+		for range 50 {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				req, _ := http.NewRequest("POST", "/products", bytes.NewBuffer(jsonReq))
+				req.Header.Set("Authorization", "Bearer "+tokenClient)
+				req.Header.Set("Content-Type", "application/json")
+
+				response := executeRequest(req, s)
+				checkResponseCode(t, http.StatusCreated, response.Code)
+			}()
+		}
+		wg.Wait()
+	})
+
+	t.Run("Close Reception", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/pvz/"+pvzResp.ID.String()+"/close_last_reception", nil)
+		req.Header.Set("Authorization", "Bearer "+tokenClient)
+		req.Header.Set("Content-Type", "application/json")
+
+		response := executeRequest(req, s)
+		checkResponseCode(t, http.StatusOK, response.Code)
+	})
+}
--- migrations/001_create_tables.up.sql
+++ migrations/001_create_tables.up.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS receptions (
     status TEXT NOT NULL,
     registration_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
+
 CREATE TABLE IF NOT EXISTS products (
     id UUID PRIMARY KEY,
     reception_id UUID NOT NULL REFERENCES Receptions(id) ON DELETE CASCADE,
@@ -28,4 +29,15 @@ CREATE TABLE IF NOT EXISTS products (
     registration_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
+CREATE INDEX idx_pvzs_registration_date ON pvzs (registration_date);
+
+CREATE INDEX idx_receptions_registration_date ON receptions (registration_date);
+
+CREATE INDEX idx_products_registration_date ON products (registration_date);
+
+CREATE INDEX idx_pvzs_id ON pvzs (id);
+
+CREATE INDEX idx_receptions_pvz_id ON receptions (pvz_id);
+
+CREATE INDEX idx_products_reception_id ON products (reception_id);
 -- +goose StatementEnd
